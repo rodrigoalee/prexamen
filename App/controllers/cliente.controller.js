8ba5f34d9017234dd5e8b7c49986bebd5227cc20
rodrigoalee <rooodrigojc@gmail.com>
2024-08-31 12:42:21 -0600

    update 2 cliente

--- App/controllers/cliente.controller.js
+++ App/controllers/cliente.controller.js
@@ -3,45 +3,48 @@ const Cliente = db.Cliente;
 
 // Crear un nuevo cliente
 exports.create = (req, res) => {
-    let cliente = {
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        nit: req.body.nit,
-        direccion: req.body.direccion,
-        telefono: req.body.telefono,
-        email: req.body.email,
-        fechaingreso: req.body.fechaingreso,
-        estatus: req.body.estatus
-    };
+    let cliente = {};
 
-    Cliente.create(cliente)
-        .then(result => {
-            res.status(201).json({
+    try {
+        // Asignar valores del cuerpo de la solicitud al objeto cliente
+        cliente.nombre = req.body.nombre;
+        cliente.apellido = req.body.apellido;
+        cliente.nit = req.body.nit;
+        cliente.direccion = req.body.direccion;
+        cliente.telefono = req.body.telefono;
+        cliente.email = req.body.email;
+        cliente.fechaingreso = req.body.fechaingreso;
+        cliente.estatus = req.body.estatus;
+
+        // Guardar el cliente en la base de datos
+        Cliente.create(cliente).then(result => {
+            res.status(200).json({
                 message: "Cliente creado exitosamente con id = " + result.id_cliente,
                 cliente: result,
             });
-        })
-        .catch(error => {
-            res.status(500).json({
-                message: "Error al crear el cliente",
-                error: error.message
-            });
         });
+    } catch (error) {
+        res.status(500).json({
+            message: "¡Error al crear el cliente!",
+            error: error.message
+        });
+    }
 };
 
 // Recuperar todos los clientes
 exports.retrieveAllClientes = (req, res) => {
     Cliente.findAll()
-        .then(clientes => {
+        .then(clienteInfos => {
             res.status(200).json({
-                message: "Información de los Clientes obtenida exitosamente",
-                clientes: clientes
+                message: "Información de todos los clientes obtenida exitosamente",
+                clientes: clienteInfos
             });
         })
         .catch(error => {
+            console.log(error);
             res.status(500).json({
-                message: "Error al obtener los clientes",
-                error: error.message
+                message: "¡Error al obtener los clientes!",
+                error: error
             });
         });
 };
@@ -51,45 +54,61 @@ exports.getClienteById = (req, res) => {
     let clienteId = req.params.id;
     Cliente.findByPk(clienteId)
         .then(cliente => {
-            if (!cliente) {
-                return res.status(404).json({
-                    message: "No se encontró el Cliente con id = " + clienteId
-                });
-            }
             res.status(200).json({
-                message: "Cliente obtenido exitosamente",
+                message: "Cliente obtenido exitosamente con id = " + clienteId,
                 cliente: cliente
             });
         })
         .catch(error => {
+            console.log(error);
             res.status(500).json({
-                message: "Error al obtener el cliente",
-                error: error.message
+                message: "¡Error al obtener el cliente!",
+                error: error
             });
         });
 };
 
 // Actualizar un cliente por ID
 exports.updateById = async (req, res) => {
-    const clienteId = req.params.id;
     try {
-        const [updated] = await Cliente.update(req.body, {
-            where: { id_cliente: clienteId }
-        });
-        if (updated) {
-            const updatedCliente = await Cliente.findByPk(clienteId);
-            res.status(200).json({
-                message: "Cliente actualizado exitosamente",
-                cliente: updatedCliente,
+        let clienteId = req.params.id;
+        let cliente = await Cliente.findByPk(clienteId);
+
+        if (!cliente) {
+            res.status(404).json({
+                message: "No se encontró el cliente con id = " + clienteId,
+                cliente: "",
+                error: "404"
             });
         } else {
-            res.status(404).json({
-                message: "No se encontró el Cliente con id = " + clienteId
+            let updatedObject = {
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                nit: req.body.nit,
+                direccion: req.body.direccion,
+                telefono: req.body.telefono,
+                email: req.body.email,
+                fechaingreso: req.body.fechaingreso,
+                estatus: req.body.estatus
+            };
+
+            let result = await Cliente.update(updatedObject, { returning: true, where: { id_cliente: clienteId } });
+
+            if (!result) {
+                res.status(500).json({
+                    message: "¡Error! No se pudo actualizar el cliente con id = " + req.params.id,
+                    error: "No se pudo actualizar",
+                });
+            }
+
+            res.status(200).json({
+                message: "Cliente actualizado exitosamente con id = " + clienteId,
+                cliente: updatedObject,
             });
         }
     } catch (error) {
         res.status(500).json({
-            message: "Error al actualizar el cliente",
+            message: "¡Error! No se pudo actualizar el cliente con id = " + req.params.id,
             error: error.message
         });
     }
@@ -97,25 +116,26 @@ exports.updateById = async (req, res) => {
 
 // Eliminar un cliente por ID
 exports.deleteById = async (req, res) => {
-    const clienteId = req.params.id;
     try {
-        const deleted = await Cliente.destroy({
-            where: { id_cliente: clienteId }
-        });
-        if (deleted) {
-            res.status(200).json({
-                message: "Cliente eliminado exitosamente",
-                id: clienteId
+        let clienteId = req.params.id;
+        let cliente = await Cliente.findByPk(clienteId);
+
+        if (!cliente) {
+            res.status(404).json({
+                message: "No existe un cliente con id = " + clienteId,
+                error: "404",
             });
         } else {
-            res.status(404).json({
-                message: "No se encontró el Cliente con id = " + clienteId
+            await cliente.destroy();
+            res.status(200).json({
+                message: "Cliente eliminado exitosamente con id = " + clienteId,
+                cliente: cliente,
             });
         }
     } catch (error) {
         res.status(500).json({
-            message: "Error al eliminar el cliente",
-            error: error.message
+            message: "¡Error! No se pudo eliminar el cliente con id = " + req.params.id,
+            error: error.message,
         });
     }
 };
