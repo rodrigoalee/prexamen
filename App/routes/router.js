10c535df0041eb47ad1dededfa84516d29432a7d
rodrigoalee <rooodrigojc@gmail.com>
2024-08-31 23:16:04 -0600

    proveedor

--- App/routes/router.js
+++ App/routes/router.js
@@ -3,7 +3,8 @@ let router = express.Router();
 
 const empleadoController = require('../controllers/empleado.controller.js');
 const departamentoController = require('../controllers/departamento.controller.js');
-;
+const proveedorController = require('../controllers/proveedor.controller.js');
+
 router.post('/api/empleados/create', empleadoController.create);
 router.get('/api/empleados/all', empleadoController.retrieveAllEmpleados);
 router.get('/api/empleados/onebyid/:id', empleadoController.getEmpleadoById);
@@ -16,6 +17,12 @@ router.get('/api/departamentos/all', departamentoController.retrieveAllDepartame
 router.get('/api/departamentos/onebyid/:id', departamentoController.getDepartamentoById);
 router.put('/api/departamentos/update/:id', departamentoController.updateById);
 router.delete('/api/departamentos/delete/:id', departamentoController.deleteById);
+
+router.post('/api/proveedores/create', proveedorController.create);
+router.get('/api/proveedores/all', proveedorController.retrieveAllProveedores);
+router.get('/api/proveedores/onebyid/:id', proveedorController.getProveedorById);
+router.put('/api/proveedores/update/:id', proveedorController.updateById);
+router.delete('/api/proveedores/delete/:id', proveedorController.deleteById);
 
 
 
